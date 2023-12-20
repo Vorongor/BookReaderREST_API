adfb27796fb08f931a299234a69ac0a739596386
Vorongor <mr.maddarknes@gmail.com>
2023-12-20 15:13:38 +0200

    nosecure

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const jwtConstants = {
+  secret: 'BookReader_SecreatKEY_Word',
+};
+
+export const googleConstsnts = {
+  clientID:
+    '241982174000-211pedfd0l1jnbaep4lvle8gbf33ahm6.apps.googleusercontent.com',
+  clientSecret: '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',
+};
+
+export const dbSets = {
+  url: 'mongodb+srv://BookReaderNest:REaUHJJD50suogBQ@bookreaderclaster.dzfrxhv.mongodb.net/bookreader',
+};
